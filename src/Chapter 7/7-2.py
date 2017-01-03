a83d236efc615b439b78c705ccea9b1415968532
flaw600 <sikand@gmail.com>
2017-01-02 22:26:10 -0600

    Completed non-trivial exercises from Chapter 7

---
+++ src/Chapter 7/7-2.py
@@ -0,0 +1,6 @@
+number = input("How many people are in your party?")
+
+if int(number) > 8:
+    print("I'm sorry, but you'll have to wait a few minutes for your table")
+else:
+    print("Your table is ready. Please follow me")
---
+++ src/Chapter 7/7-3.py
@@ -0,0 +1,3 @@
+number = input("Please enter a number and i'll tell you if that number is a multiple of 10 or not")
+
+print(int(number) % 10 == 0)
